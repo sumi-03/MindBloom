353ba1980dda2cf2d71f7eb1ca208916fcf4f675
sumi <lsmkoo03@naver.com>
2025-06-04 11:24:37 +0900

    Initial Commit

---
+++ MindBloom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MindBloom
+//
+//  Created by 임수미 on 6/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
